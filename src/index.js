a40d75bb9754361a83552ab6b5d1f0ca2d245c8a
Robert Tenney <rob10esf@gmail.com>
2018-01-05 20:44:22 -0500

    Minor corrections for building

--- src/index.js
+++ src/index.js
@@ -28,8 +28,8 @@ const createWindow = () => {
   // Open the DevTools.
   if (process.env.NODE_ENV === 'development') {
     installExtension([REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS])
-      .then(name => console.log(`Added Extension:  ${name}`))
-      .catch(err => console.log('An error occurred: ', err));
+      .then(name => console.info(`Added Extension:  ${name}`))
+      .catch(err => console.error('An error occurred: ', err));
     mainWindow.webContents.openDevTools();
   }
 
